3e716a95048975398e2d97ce60bf05d92e308348
user <jeffdplante@aol.com>
2023-03-01 16:14:22 -0800

    Add styling and logo

--- src/modules/elements.js
+++ src/modules/elements.js
@@ -2,6 +2,10 @@ const elements = (() => {
   function createHeader() {
     const header = document.createElement('header');
     header.classList = 'header';
+    const logo = document.createElement('div');
+    logo.id = 'logo';
+    logo.textContent = 'ToDo';
+    header.appendChild(logo);
     return header;
   }
 
